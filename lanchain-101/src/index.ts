b7e68149c858b0ed71b6705739340d51a398e745
Oussama Moussaoui <omoussaoui1337@gmail.com>
2025-09-04 15:15:15 +0100

    chore: create terminal chat

--- lanchain-101/src/index.ts
+++ lanchain-101/src/index.ts
@@ -2,7 +2,11 @@ import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
 import { config } from './config';
 import { urlToBase64Url } from './common/base64-url';
 import { PromptTemplate, ChatPromptTemplate } from '@langchain/core/prompts';
-import rl from 'readline';
+import readline from 'readline';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 main();
 async function main() {
@@ -12,19 +16,36 @@ async function main() {
   });
 
   const prompt = ChatPromptTemplate.fromMessages([
-    ['system', 'If the human message a yes/no question, answer with yes or no. Otherwise, don\'t answer.'],
+    [
+      'system',
+      "If the human message a yes/no question, answer with yes or no. Otherwise, answer with some sorry message.",
+    ],
     ['human', '{input}'],
   ]);
 
   const yesOrNoModel = prompt.pipe(model);
 
-  const response = await yesOrNoModel.invoke({
-    input: 'how is the weather today?',
-  });
+  while (true) {
+    const input = await getInput();
+    if (!input) break;
 
-  console.log(JSON.stringify(response, null, 2));
+    const response = await yesOrNoModel.invoke({
+      input,
+    });
+  
+    console.log(response.text);
+  }
+
 }
 
-// async function prompt() {
-//   rl.question()
-// }
+async function getInput() {
+  return new Promise<string | null>((resolve, reject) => {
+    rl.question('> ', async (input) => {
+      if (input === 'exit') {
+        rl.close();
+        return resolve(null);
+      }
+      return resolve(input);
+    });
+  });
+}
