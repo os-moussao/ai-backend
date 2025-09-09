a4a9be94b3deed4f8fc74fe6acaaf78379532361
Oussama Moussaoui <omoussaoui1337@gmail.com>
2025-09-09 11:01:15 +0100

    chore: implement starter for langgraph

---
+++ langgraph/src/common/models.registry.ts
@@ -0,0 +1,29 @@
+import {
+  ChatGoogleGenerativeAI,
+  GoogleGenerativeAIChatInput,
+} from '@langchain/google-genai';
+import { config } from '../config';
+
+const GEMINI_MODELS = [
+  'gemini-2.5-pro',
+  'gemini-2.5-flash',
+  'gemini-2.5-flash-lite',
+  'gemini-2.0-flash-preview-image-generation',
+  'imagen-4.0-generate-preview-06-06',
+] as const;
+
+export const modelsRegistry = {
+  gemini: {
+    availableModels: GEMINI_MODELS,
+    new(
+      model: (typeof GEMINI_MODELS)[number],
+      options: Omit<GoogleGenerativeAIChatInput, 'apiKey' | 'model'> = {}
+    ) {
+      return new ChatGoogleGenerativeAI({
+        apiKey: config.GEMINI_API_KEY,
+        model,
+        ...options,
+      });
+    },
+  },
+};
---
+++ langgraph/src/index.ts
@@ -0,0 +1,14 @@
+import { getInput } from './common/io.utils';
+
+main();
+
+async function main() {
+  while (true) {
+    const input = await getInput();
+    if (!input) break;
+
+    // const response = await getResponse()
+    // console.log(response)
+  }
+}
+
